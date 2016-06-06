5f761c878eec7ead990409ef38baaa6d7906575d
Mslaster <moshe.laster@mail.yu.edu>
2016-06-06 14:06:15 -0400

    Update advanced_python_csv.py

--- python/advanced_python_csv.py
+++ python/advanced_python_csv.py
@@ -1,1 +1,20 @@
-PLACE YOUR CODE HERE
+```
+import csv
+import pandas as pd
+
+faculty=pd.read_csv('faculty.csv', index_col=0)
+
+def emails(l):
+    address_list=[]    
+    for address in faculty[' email']:
+        address_list.append(address)
+    return address_list
+
+fac_emails=emails(faculty)
+    
+a=open('emails.csv','w')
+b=csv.writer(a)
+for email in fac_emails:
+    b.writerow([email])
+a.close
+```
