3892bf5bb5ae34f3644b4c212ef3d37d7c1d5b0c
Mslaster <moshe.laster@mail.yu.edu>
2016-06-07 12:50:35 -0400

    Update advanced_python_dict.py

--- python/advanced_python_dict.py
+++ python/advanced_python_dict.py
@@ -1,1 +1,14 @@
-PLACE YOUR CODE HERE
+
+```
+import csv
+import pandas as pd
+
+faculty=pd.read_csv('faculty.csv', index_col=0)
+# 
+faculty_dict={}
+for name in faculty.index:    
+    if name.split()[-1] not in faculty_dict.keys():    
+        faculty_dict[name.split()[-1]]=[[faculty[' degree'][name], faculty[' title'][name], faculty[' email'][name]]]
+    else:
+       faculty_dict[name.split()[-1]].append([faculty[' degree'][name], faculty[' title'][name], faculty[' email'][name]])  
+  ```
